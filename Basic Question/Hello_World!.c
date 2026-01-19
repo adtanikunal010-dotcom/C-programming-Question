f0995a4740eea2ceb0d3a1b455d7d2c208b96b9f
adtanikunal010-dotcom <adtanikunal010@gmail.com>
2026-01-19 19:59:30 +0530

    first commit

---
+++ Basic Question/Hello_World!.c
@@ -0,0 +1,14 @@
+/*
+Problem - Printing Hello World!
+Platform - HackerRank
+Summary - Printing Hello World!
+*/
+
+#include <stdio.h>
+
+int main() 
+{
+    printf("Hello World!");
+
+    return 0;
+}
