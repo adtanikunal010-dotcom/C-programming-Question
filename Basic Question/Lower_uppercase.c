44d96057e735c9516c0fc8ad13acdf99dc94c081
adtanikunal010-dotcom <adtanikunal010@gmail.com>
2026-01-20 12:53:21 +0530

    first commit

---
+++ Basic Question/Lower_uppercase.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char lowercase;
+    char uppercase;
+
+    scanf("%c", &lowercase);
+    uppercase= lowercase - 32;
+    
+    printf("The uppercase of %c is %c" ,lowercase , uppercase);
+    return 0;
+}
+ 
