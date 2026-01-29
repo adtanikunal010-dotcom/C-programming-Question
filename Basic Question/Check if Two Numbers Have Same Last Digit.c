a74abb09ec9d47194774cd8597c11940e1ae71e6
adtanikunal010-dotcom <adtanikunal010@gmail.com>
2026-01-30 00:50:29 +0530

    first commit

---
+++ Basic Question/Check if Two Numbers Have Same Last Digit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    scanf("%d %d", &num1,&num2);
+    int last1 = num1 -(num1 / 10)*10;
+    int last2 = num2 -(num2 /10)*10;
+    
+    if(last1==last2){
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+ 
+    return 0;
+}
